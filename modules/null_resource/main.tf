87c19e2d2d2c27d7a167f246eb1acbada9bf0551
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-02-13 17:48:34 +0200

    Update main.tf

--- modules/null_resource/main.tf
+++ modules/null_resource/main.tf
@@ -1,3 +1,35 @@
+
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "2.1.1"
+        }
+    }
+}
+data "scalr_current_account" "data_acc" {}
+
+data "scalr_environment" "data_env" {
+#account_id = data.scalr_current_account.data_acc
+  id = "env-ugnpuhbhe73a4e0"  # optional, can only use id or name for the environment filter, if both are used there will be a conflict.
+}
+
+
+resource "scalr_variable" "new_var" {
+  count = 1
+  key            = "var" #-${count.index}
+  value          = "TRACE"
+  category       = "terraform"
+  environment_id = data.scalr_environment.data_env.id
+}
+
+resource "scalr_variable" "var_new2" {
+  key            = "key1"
+  value          = "1"
+  category       = "shell"
+  environment_id = data.scalr_environment.data_env.id
+}
+
 #SIMPLE NULL RESOURCE CONFIGURATION
 resource "null_resource" "example" {
   triggers = {
@@ -13,3 +45,5 @@ resource "null_resource" "example" {
 output "ensure_env" {
   value = "Hello from ${var.environment}!"
 }
+
+
